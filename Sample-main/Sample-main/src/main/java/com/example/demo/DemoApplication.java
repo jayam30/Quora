5a8ef1884e1ea3c8b5fefbe6bb0b6ab6baaf554f
jayam30 <panditjayam@gmail.com>
2025-04-28 23:39:33 +0530

    Initial commit

---
+++ Sample-main/Sample-main/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+		System.out.println("This is Demo project...");
+		System.out.println("1");
+		System.out.println("2");
+		System.out.println("3");
+		System.out.println("4");
+		System.out.println("5");
+		System.out.println("AG made changes");
+		System.out.println("Ag made changes second time");
+
+		System.out.println("gv made changes first time");
+		System.out.println("night time changes !!!!");
+		
+	}
+
+}
